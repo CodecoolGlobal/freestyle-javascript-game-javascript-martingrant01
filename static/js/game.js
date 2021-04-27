c8a665a089640b1cee44ae01821867fd50a0424f
Zoltán Baranyay <zbaranyay@gmail.com>
2021-04-27 15:09:51 +0200

    feat: implement snake movement

--- static/js/game.js
+++ static/js/game.js
@@ -1,23 +1,77 @@
-function initGame() {
+window.onload = function initGame() {
+        let fields = document.querySelectorAll('.table_col');
 
-        initCreateBorder()
+        createSnake(fields)
+        snakeMovement(fields)
+
     // Your game can start here, but define separate functions, don't write everything in here :)
+        function createSnake (fields) {
 
+                for (let field of fields) {
+                        if (field.dataset.row === '5' && field.dataset.col === '5') {
+                                field.classList.add('snake');
+                                field.setAttribute('id', 'snake-head');
+                        }
+                }
+        }
+
+        function snakeMovement (fields) {
+                function move (fields, direction, change) {
+                        let snakeHead = document.querySelector('#snake-head');
+                        let snakeHeadPosition = {
+                                row: parseInt(snakeHead.dataset.row),
+                                col: parseInt(snakeHead.dataset.col)
+                        }
+
+                        if (direction === 'col' ? snakeHeadPosition.col += change : snakeHeadPosition.row += change);
+
+                        snakeHead.classList.remove('snake');
+                        snakeHead.removeAttribute('id');
+
+                        console.log(snakeHeadPosition)
+
+                        for (let field of fields) {
+                                if (field.dataset.col == snakeHeadPosition.col && field.dataset.row == snakeHeadPosition.row) {
+                                        console.log(snakeHeadPosition)
+                                        field.classList.add('snake');
+                                        field.setAttribute('id', 'snake-head');
+                                }
+                        }
+                }
+
+                window.addEventListener('keydown', function (event){
+                        if (event.defaultPrevented) {
+                                return;
+                        }
+
+                        switch (event.code) {
+                                case 'KeyS':
+                                case 'ArrowDown':
+                                        console.log('back');
+                                        move(fields,'row', 1);
+                                        break;
+                                case 'KeyW':
+                                case 'ArrowUp':
+                                        console.log('up');
+                                        move(fields,'row', -1);
+                                        break;
+                                case 'KeyA':
+                                case 'ArrowLeft':
+                                        console.log('left');
+                                        move(fields,'col', -1);
+                                        break;
+                                case 'KeyD':
+                                case 'ArrowRight':
+                                        console.log('right');
+                                        move(fields,'col', 1);
+                                        break;
+                        }
+                },
+
+        )};
 }
 
-/*
-function initCreateBorde() {
-    let last_row = document.querySelectorAll('.table_row');
 
-    let border_cells = document.querySelectorAll(`[data-row= '0'] [data-row= ${CSS.escape((last_row.length - 1).toString())}]`);
 
-    for (let border_cell of border_cells) {
-            border_cell.classList.add('table_border');
-        console.log(border_cell.dataset.row);
 
-    }
 
-}
-*/
-
-initGame();
