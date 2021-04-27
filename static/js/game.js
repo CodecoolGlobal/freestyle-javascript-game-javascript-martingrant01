cc9b5204f6e576fbe96c88bb662bdc0c2675431a
Stark Máté <strkmt@gmail.com>
2021-04-27 16:43:37 +0200

    feat: implement time based movement

--- static/js/game.js
+++ static/js/game.js
@@ -28,44 +28,56 @@ window.onload = function initGame() {
                         snakeHead.classList.remove('snake');
                         snakeHead.removeAttribute('id');
 
-                        console.log(snakeHeadPosition)
+                        //console.log(snakeHeadPosition)
 
                         for (let field of fields) {
                                 if (field.dataset.col == snakeHeadPosition.col && field.dataset.row == snakeHeadPosition.row) {
-                                        console.log(snakeHeadPosition)
+                                        //console.log(snakeHeadPosition)
                                         field.classList.add('snake');
                                         field.setAttribute('id', 'snake-head');
                                 }
                         }
                 }
-
+                let timer;
                 window.addEventListener('keydown', function (event){
-                        if (event.defaultPrevented) {
-                                return;
-                        }
+                        event.preventDefault();
 
-                        switch (event.code) {
-                                case 'KeyS':
-                                case 'ArrowDown':
-                                        console.log('back');
-                                        move(fields,'row', 1);
-                                        break;
-                                case 'KeyW':
-                                case 'ArrowUp':
-                                        console.log('up');
-                                        move(fields,'row', -1);
-                                        break;
-                                case 'KeyA':
-                                case 'ArrowLeft':
-                                        console.log('left');
-                                        move(fields,'col', -1);
-                                        break;
-                                case 'KeyD':
-                                case 'ArrowRight':
-                                        console.log('right');
-                                        move(fields,'col', 1);
-                                        break;
+                        console.log(event.code)
+                        function new_key(event) {
+                                let keykeys = ['KeyS', 'ArrowDown', 'KeyW', 'ArrowUp', 'KeyA', 'ArrowLeft', 'KeyD', 'ArrowRight']
+                                for (let keykey of keykeys) {
+                                        if (event.code == keykey) clearInterval(timer);
+                                }
+
                         }
+                        new_key(event);
+                        timer = setInterval( function() {
+                                switch (event.code) {
+                                        case 'KeyS':
+                                        case 'ArrowDown':
+                                                //console.log('back');
+                                                move(fields,'row', 1);
+                                                break;
+                                        case 'KeyW':
+                                        case 'ArrowUp':
+                                                //console.log('up');
+                                                move(fields,'row', -1);
+                                                break;
+                                        case 'KeyA':
+                                        case 'ArrowLeft':
+                                                //console.log('left');
+                                                move(fields,'col', -1);
+                                                break;
+                                        case 'KeyD':
+                                        case 'ArrowRight':
+                                                //console.log('right');
+                                                move(fields,'col', 1);
+                                                break;
+                                }
+                        }, 200)
+
+
+
                 },
 
         )};
